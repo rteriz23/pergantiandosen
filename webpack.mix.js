a11281233f8d03fe40de5f35c9460001c4ef2ca1
ruly <rulyce23@gmail.com>
2026-05-21 10:27:28 +0700

    fix : resolve path issue on actions and disable webpack progress bar

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.options({
+    progress: false
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
